5aacf74de0ef8bb50c6a36095ec6399921fa114e
Ebenezer2077 <tobigcb547@outlook.de>
2025-01-02 16:45:05 +0100

    axe is rougly designed

--- 13_ThreeJS_Physik_VR/js/geometry.mjs
+++ 13_ThreeJS_Physik_VR/js/geometry.mjs
@@ -2,6 +2,7 @@ import { mergeGeometries } from '../../99_Lib/jsm/utils/BufferGeometryUtils.js';
 import * as THREE from '../../99_Lib/three.module.min.js';
 
 const geometries = [
+    //createAxeHead(),
     createAxe(),
     new THREE.BoxGeometry(0.25, 0.25, 0.25),
     new THREE.ConeGeometry(0.1, 0.4, 64),
@@ -27,11 +28,41 @@ export function add(i, parent, x = 0, y = 0, z = 0) {
     return object;
 }
 
-function createAxe() {
-    const first = new THREE.ConeGeometry(0.1, 0.4, 64);
-    const second = new THREE.BoxGeometry(0.25, 0.25, 0.25);
+function createAxeHead() {
+    const shape = new THREE.Shape();
+    shape.moveTo(0,0);
+    shape.lineTo(0,0);
+    shape.lineTo(0,0.1);
+    shape.lineTo(0.3,0.05);
+    shape.lineTo(0,0);
 
-    const merged = mergeGeometries([first, second])
+    const extrudeSettings = {
+        steps: 2,
+        depth: 0.1,
+        bevelEnabled: true,
+        bevelThickness: 0.1,
+        bevelSize: 0.1,
+        bevelOffset: 0,
+        bevelSegments: 1
+    };
+
+    const Extr = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    Extr.translate(0,0,0);
+    Extr.rotateX(Math.PI/2);
+    return Extr;
+}
+
+function createAxe() {
+    const AxeHandle = new THREE.CylinderGeometry(0.05, 0.05, 1.2, 64);
+    //  linksrechts     hochrunter      vorzurück
+    AxeHandle.translate(0,0.65,0.05);
+
+    const AxeHead = new THREE.BoxGeometry(0.25, 0.25, 0.05);
+    AxeHead.translate(0.1,0.6,0);
+    
+
+    const merged = mergeGeometries([AxeHandle.toNonIndexed(), createAxeHead()])
+    
     return merged;
     
     
